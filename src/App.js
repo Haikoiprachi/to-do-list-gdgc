ad73f1b289991fe2f3ea9bfd38fbb9787b0131a1
haikoiprachi <prachic861@gmail.com>
2024-10-31 01:38:38 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+
+  const handleAddTodo = () => {
+    if (inputValue.trim() === '') return;
+    setTodos([...todos, { text: inputValue, completed: false }]);
+    setInputValue('');
+  };
+
+  const handleToggleTodo = (index) => {
+    const newTodos = todos.map((todo, i) =>
+      i === index ? { ...todo, completed: !todo.completed } : todo
+    );
+    setTodos(newTodos);
+  };
+
+  const handleDeleteTodo = (index) => {
+    const newTodos = todos.filter((_, i) => i !== index);
+    setTodos(newTodos);
+  };
+
+  return (
+    <div className="app">
+      <h1>Todo App</h1>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        placeholder="Add a new todo"
+      />
+      <button onClick={handleAddTodo}>Add</button>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index} style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}>
+            <span onClick={() => handleToggleTodo(index)}>{todo.text}</span>
+            <button onClick={() => handleDeleteTodo(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
